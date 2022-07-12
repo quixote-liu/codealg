f1a2755b578a1e69dfdbc52b356b9ec75510a3eb
lcs <lcs.shun@foxmail.com>
2022-07-12 20:46:59 +0800

    opt

--- aes.go
+++ aes.go
@@ -4,17 +4,59 @@ import (
 	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
+	"crypto/hmac"
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/hex"
+	"io"
+	mrand "math/rand"
+	"strconv"
+	"sync"
+	"time"
 )
 
-func AESECBEncrypt(plaintext, key []byte) ([]byte, error) {
-	return nil, nil
+// ----------------- ECB --------------------------------
+
+func AesECBEncrypt(plaintext, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	length := (len(plaintext) + aes.BlockSize) / aes.BlockSize
+	plain := make([]byte, length*aes.BlockSize)
+	copy(plain, plaintext)
+	pad := byte(len(plain) - len(plaintext))
+	for i := len(plaintext); i < len(plain); i++ {
+		plain[i] = pad
+	}
+	encrypted := make([]byte, len(plain))
+	blockSize := block.BlockSize()
+	for bs, be := 0, blockSize; bs <= len(plaintext); bs, be = bs+blockSize, be+blockSize {
+		block.Encrypt(encrypted[bs:be], plain[bs:be])
+	}
+	return encrypted, nil
 }
 
-func AESECBDecrypt(ciphertext, key []byte) ([]byte, error) {
-	return nil, nil
+func AesECBDecrypt(ciphertext, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	decrypted := make([]byte, len(ciphertext))
+	blockSize := block.BlockSize()
+	for bs, be := 0, blockSize; bs < len(ciphertext); bs, be = bs+blockSize, be+blockSize {
+		block.Decrypt(decrypted, ciphertext)
+	}
+	trim := 0
+	if len(decrypted) > 0 {
+		trim = len(decrypted) - int(decrypted[len(decrypted)-1])
+	}
+	return decrypted[:trim], nil
 }
 
-func AESCBCEncrypt(plaintext, key []byte) ([]byte, error) {
+// ----------------- CBC --------------------------------
+
+func AesCBCEncrypt(plaintext, key []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
@@ -27,7 +69,7 @@ func AESCBCEncrypt(plaintext, key []byte) ([]byte, error) {
 	return crypted, nil
 }
 
-func AESCBCDecrypt(ciphertext, key []byte) ([]byte, error) {
+func AesCBCDecrypt(ciphertext, key []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
@@ -40,14 +82,6 @@ func AESCBCDecrypt(ciphertext, key []byte) ([]byte, error) {
 	return originData, nil
 }
 
-func GenAESKey() string {
-	key := make([]byte, 16)
-
-	// TODO: generate aes key
-
-	return string(key)
-}
-
 func pkcs5Padding(plaintext []byte, blockSize int) []byte {
 	padding := blockSize - len(plaintext)%blockSize
 	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
@@ -58,4 +92,54 @@ func pkcs5UnPadding(origData []byte) []byte {
 	length := len(origData)
 	unpadding := int(origData[length-1])
 	return origData[:(length - unpadding)]
+}
+
+// ----------------- CFB --------------------------------
+
+func AesCFBEncrypt(plaintext, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	encrypted := make([]byte, aes.BlockSize+len(plaintext))
+	iv := encrypted[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, err
+	}
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(encrypted[aes.BlockSize:], plaintext)
+	return encrypted, nil
+}
+
+func AesCFBDecrypt(ciphertext, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+	stream := cipher.NewCFBDecrypter(block, iv)
+	stream.XORKeyStream(ciphertext, ciphertext)
+	return ciphertext, nil
+}
+
+// ----------------- common --------------------------------
+
+var mu sync.Mutex
+
+func GenAesRandomKey() string {
+	mu.Lock()
+	defer mu.Unlock()
+
+	random := time.Now().UnixMilli() + mrand.Int63()
+	key := strconv.FormatInt(random, 10)
+	hmacNew := hmac.New(md5.New, []byte(key))
+	_, _ = hmacNew.Write([]byte(key))
+	return hex.EncodeToString(hmacNew.Sum([]byte("")))
+}
+
+func GenAesKey(key string) string {
+	hmacNew := hmac.New(md5.New, []byte(key))
+	_, _ = hmacNew.Write([]byte(key))
+	return hex.EncodeToString(hmacNew.Sum([]byte("")))
 }
--- aes_test.go
+++ aes_test.go
@@ -6,15 +6,157 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAESCBCEncryptAndDecrypt(t *testing.T) {
-	key := "ABCDEFGHIJKLMNOP"
-	plaintext := "hello, world"
+func TestGenAesRandomKey(t *testing.T) {
+	key1 := GenAesRandomKey()
+	assert.NotEqual(t, "", key1)
+	key2 := GenAesRandomKey()
+	assert.NotEqual(t, key1, key2)
+}
 
-	ciphertext, err := AESCBCEncrypt([]byte(plaintext), []byte(key))
-	assert.Nil(t, err)
+func TestGenAESKey(t *testing.T) {
+	key1 := GenAesKey("hello, world")
+	assert.NotEqual(t, "", key1)
+	key2 := GenAesKey("hello, world")
+	assert.Equal(t, key1, key2)
+}
 
-	actual, err := AESCBCDecrypt(ciphertext, []byte(key))
-	assert.Nil(t, err)
+func TestAesCBCEncrypt(t *testing.T) {
+	tests := []struct {
+		name    string
+		key     string
+		plain   string
+		wantErr bool
+	}{
+		{"should return err with empty key", "", "hello, world", true},
+		{"should return err with incorrect key", "insdfa", "hello, world", true},
+		{"should pass  with correct key", GenAesRandomKey(), "hello, world", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ciphertext, err := AesCBCEncrypt([]byte(tt.plain), []byte(tt.key))
+			if tt.wantErr && err != nil {
+				return
+			}
 
-	assert.Equal(t, plaintext, string(actual))
+			actual, _ := AesCBCDecrypt(ciphertext, []byte(tt.key))
+			assert.Equal(t, tt.plain, string(actual))
+		})
+	}
+}
+
+func TestAesCBCDecrypt(t *testing.T) {
+	tests := []struct {
+		name    string
+		key     string
+		plain   string
+		wantErr bool
+	}{
+		{"should return err with empty key", "", "hello, world", true},
+		{"should return err with incorrect key", "insdfa", "hello, world", true},
+		{"should pass  with correct key", GenAesRandomKey(), "hello, world", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ciphertext, _ := AesCBCEncrypt([]byte(tt.plain), []byte(tt.key))
+			actual, err := AesCBCDecrypt(ciphertext, []byte(tt.key))
+			if tt.wantErr && err != nil {
+				return
+			}
+			assert.Equal(t, tt.plain, string(actual))
+		})
+	}
+}
+
+func TestAesECBEncrypt(t *testing.T) {
+	tests := []struct {
+		name    string
+		key     string
+		plain   string
+		wantErr bool
+	}{
+		{"should return err with empty key", "", "hello, world", true},
+		{"should return err with incorrect key", "insdfa", "hello, world", true},
+		{"should pass  with correct key", GenAesRandomKey(), "hello, world", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ciphertext, err := AesECBEncrypt([]byte(tt.plain), []byte(tt.key))
+			if tt.wantErr && err != nil {
+				return
+			}
+
+			actual, _ := AesECBDecrypt(ciphertext, []byte(tt.key))
+			assert.Equal(t, tt.plain, string(actual))
+		})
+	}
+}
+
+func TestAesECBDecrypt(t *testing.T) {
+	tests := []struct {
+		name    string
+		key     string
+		plain   string
+		wantErr bool
+	}{
+		{"should return err with empty key", "", "hello, world", true},
+		{"should return err with incorrect key", "insdfa", "hello, world", true},
+		{"should pass  with correct key", GenAesRandomKey(), "hello, world", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ciphertext, _ := AesECBEncrypt([]byte(tt.plain), []byte(tt.key))
+			actual, err := AesECBDecrypt(ciphertext, []byte(tt.key))
+			if tt.wantErr && err != nil {
+				return
+			}
+			assert.Equal(t, tt.plain, string(actual))
+		})
+	}
+}
+
+func TestAesCFBEncrypt(t *testing.T) {
+	tests := []struct {
+		name    string
+		key     string
+		plain   string
+		wantErr bool
+	}{
+		{"should return err with empty key", "", "hello, world", true},
+		{"should return err with incorrect key", "insdfa", "hello, world", true},
+		{"should pass  with correct key", GenAesRandomKey(), "hello, world", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ciphertext, err := AesCFBEncrypt([]byte(tt.plain), []byte(tt.key))
+			if tt.wantErr && err != nil {
+				return
+			}
+
+			actual, _ := AesCFBDecrypt(ciphertext, []byte(tt.key))
+			assert.Equal(t, tt.plain, string(actual))
+		})
+	}
+}
+
+func TestAesCFBDecrypt(t *testing.T) {
+	tests := []struct {
+		name    string
+		key     string
+		plain   string
+		wantErr bool
+	}{
+		{"should return err with empty key", "", "hello, world", true},
+		{"should return err with incorrect key", "insdfa", "hello, world", true},
+		{"should pass  with correct key", GenAesRandomKey(), "hello, world", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ciphertext, _ := AesCFBEncrypt([]byte(tt.plain), []byte(tt.key))
+			actual, err := AesCFBDecrypt(ciphertext, []byte(tt.key))
+			if tt.wantErr && err != nil {
+				return
+			}
+			assert.Equal(t, tt.plain, string(actual))
+		})
+	}
 }
